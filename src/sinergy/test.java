0d369cb74bdf6b401e9621da2a3b1affe03bdd75
VladimirJG <vladimi85@gmail.com>
2023-03-07 12:56:40 +0300

    7 HW

--- src/sinergy/test.java
+++ src/sinergy/test.java
@@ -4,23 +4,40 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class test {
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<>();
         ArrayList<String> list2 = new ArrayList<>();
-        Collections.addAll(list, "Каша Машев", "Jora Bora", "Input Strimov");
+        ArrayList<String> list3 = new ArrayList<>();
+        Collections.addAll(list, "Lаша Машев", "Jora Bora", "Input Strimov", "Lран Упашев");
         System.out.println(list);
-      /*  for (int i = 0; i < list.size(); i++) {
+       /* for (int i = 0; i < list.size(); i++) {
             String[] m = list.get(i).split(" ");
-            list2.add(m[1]);
+            char[] str = m[0].toCharArray();
+            if (str[0] == 'L'){
+                list2.add(String.valueOf(str));
+            }
         }
         System.out.println(list2);*/
+//3
+        //list.stream().map(s->s.split(" ")[0]).toList().stream().map(x->x.split("")[0].equals("L"));
+        /*list.stream().map(s->s.split(" ")[0]).toList().stream().dropWhile(x -> x.equals("L"))
+                .forEach(list2::add);
+        list2.stream().filter(s -> s.contains("L")).forEach(list3::add);
+        System.out.println(list3);*/
 
+        list.stream().map(s -> s.split(" ")[0]).filter(s -> s.contains("L")).forEach(list2::add);
+        System.out.println(list2);
+
+//1
         //list.forEach(s -> System.out.println(Arrays.toString(new String[]{s.split(" ")[1]})));
         // list.forEach(s-> list2.add(Arrays.toString(new String[]{s.split(" ")[1]})));
-        list.stream().map(s -> s.split(" ")[1]).collect(Collectors.toList()).forEach(list2::add);
 
-        System.out.println(list2);
+  //2
+       /* list.stream().map(s -> s.split(" ")[1]).collect(Collectors.toList()).forEach(list2::add);
+        System.out.println(list2);*/
+
     }
 }
--- src/utility/Lambda.java
+++ src/utility/Lambda.java
@@ -9,7 +9,7 @@ import java.util.stream.IntStream;
 
 public class Lambda {
     public static void main(String[] args) throws Exception {
-      /*  BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
         ArrayList<String> list = new ArrayList<>();
         int i = 0;
@@ -17,14 +17,13 @@ public class Lambda {
             Collections.addAll(list, reader.readLine());
             i++;
         }
-        list.stream().filter(s -> s.contains("д")).forEach(s -> System.out.println(s + " - " + s.length()));
-        list.stream().takeWhile(s -> s.length() <= 5).forEach(s -> System.out.println(s + " - " + s.length()));
+       // list.stream().filter(s -> s.contains("д")).forEach(s -> System.out.println(s + " - " + s.length()));
+       // list.stream().takeWhile(s -> s.length() <= 5).forEach(s -> System.out.println(s + " - " + s.length()));
         list.stream().dropWhile(s -> s.length() <= 5).forEach(s -> System.out.println(s + " - " + s.length()));
-        */
 
-        int [] list1 = {5, 6, 15, 25, 8};
+       /* int [] list1 = {5, 6, 15, 25, 8};
 
         int odd = (int) IntStream.of(list1).filter(o -> (o % 2 != 0)).count();
-        System.out.println(odd);
+        System.out.println(odd);*/
     }
 }
