b958e2cd8dbb4dafa963d1ff33b132709b01d1ad
VladimirJG <vladimi85@gmail.com>
2023-03-14 15:01:54 +0300

    tin 12 start

---
+++ src/tin/task_12.java
@@ -0,0 +1,23 @@
+package tin;
+
+import java.util.Scanner;
+
+public class task_12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int monet = 1;
+        boolean sumTrue;
+        if (a + b + c == n) {
+            sumTrue = true;
+        }
+        sumTrue = false;
+        int count = 0;
+        if (sumTrue = true) {
+
+        }
+    }
+}
