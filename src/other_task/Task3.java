fc1b9a8b8e69a77080ab15e5c256de212187ffd5
VladimirJG <vladimi85@gmail.com>
2023-03-17 10:19:48 +0300

    LeetCode task3

---
+++ src/other_task/Task3.java
@@ -0,0 +1,38 @@
+package other_task;
+//Input: s = "abcabcbb"
+//Output: 3
+//Explanation: The answer is "abc", with the length of 3.
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Input: s = "bbbbb"
+//Output: 1
+//Explanation: The answer is "b", with the length of 1.
+//Example 3:
+//
+//Input: s = "pwwkew"
+//Output: 3
+//Explanation: The answer is "wke", with the length of 3.
+//Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+public class Task3 {
+    public static void main(String[] args) {
+        String s = "pwwkew";
+        StringBuilder newS = new StringBuilder();
+        List<String> list = new ArrayList<>();
+        list.add("");
+        for (int i = 0; i < s.length(); i++) {
+            String symbol = String.valueOf(s.charAt(i));
+            if (newS.toString().contains(symbol)) {
+                if (list.get(0).length() < newS.length()) {
+                    list.set(0, newS.toString());
+                }
+                newS = new StringBuilder();
+                newS.append(symbol);
+            } else {
+                newS.append(symbol);
+            }
+        }
+        System.out.println(list.get(0).length());
+    }
+}
