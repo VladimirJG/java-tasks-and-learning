f81aff1aaa5c87261baaf9fc2a812de84c8d89ce
VladimirJG <vladimi85@gmail.com>
2023-04-05 15:34:01 +0300

    LeetCode task 9 finished

--- src/other_task/Task9Polindrome.java
+++ src/other_task/Task9Polindrome.java
@@ -8,13 +8,13 @@ import java.util.List;
 //Explanation: 121 reads as 121 from left to right and from right to left.
 public class Task9Polindrome {
     public static void main(String[] args) {
-        int x = 1221;
+        int x = 1234567899;
         System.out.println(isPalindrome(x));
     }
 
     public static boolean isPalindrome(int x) {
         boolean tru = false;
-        if (x<0){
+        if (x < 0) {
             return tru;
         }
         String y = String.valueOf(x);
@@ -23,8 +23,8 @@ public class Task9Polindrome {
             String o = String.valueOf(y.charAt(i));
             m += Integer.parseInt(o);
         }
-        int i = Integer.parseInt(m);
-        if (i == x) {
+        long t = Long.parseLong(m);
+        if (t == x) {
             tru = true;
         }
         return tru;
