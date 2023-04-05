ca1aed12fe919b53065b558197071933ee336ab0
VladimirJG <vladimi85@gmail.com>
2023-04-05 08:16:03 +0300

    LeetCode task8

--- src/other_task/task8.java
+++ src/other_task/task8.java
@@ -18,21 +18,25 @@ import java.util.List;
 //Explanation: There is no common prefix among the input strings.
 public class task8 {
     public static void main(String[] args) {
-        String[] strs = {"flower", "flow", "flight","floor"};
+        String[] strs = {"flower", "flow", "flight", "floor"};
         System.out.println(longestCommonPrefix(strs));
     }
+
     public static String longestCommonPrefix(String[] strs) {
         String str1 = strs[0];
         String str2 = "";
         for (int i = 1; i < strs.length; i++) {
             for (int i1 = 0; i1 < strs[i].length(); i1++) {
-                String m = String.valueOf(strs[i].charAt(i1));
-                if (str1.contains(m)){
-                    str2+=m;
+                if (i1 < str1.length()) {
+                    String substr1 = String.valueOf(strs[i].charAt(i1));
+                    String substr2 = String.valueOf(str1.charAt(i1));
+                    if (substr1.equals(substr2)) {
+                        str2 += substr1;
+                    }
                 }
             }
-            str1=str2;
-            str2="";
+            str1 = str2;
+            str2 = "";
         }
         return str1;
     }
