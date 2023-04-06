f96d740959d8240da4d6209524df7c063ea717fa
VladimirJG <vladimi85@gmail.com>
2023-04-06 08:12:19 +0300

    LeetCode task 20 finished

---
+++ src/other_task/Task20ValidParentheses.java
@@ -0,0 +1,34 @@
+package other_task;
+
+//Учитывая строку s, содержащую только символы '(', ')', '{', и '}',
+// определите, допустима ли входная строка.'['']'
+//
+//Входная строка действительна, если:
+//
+//Открытые скобки должны быть закрыты однотипными скобками.
+//Открытые скобки должны быть закрыты в правильном порядке.
+//Каждой закрывающей скобке соответствует открытая скобка того же типа.
+public class Task20ValidParentheses {
+    public static void main(String[] args) {
+        String s = "{}()";
+        System.out.println(isValid(s));
+    }
+
+    public static boolean isValid(String s) {
+        boolean state = false;
+        for (int i = 0; i < s.length(); i++) {
+            char symbol = s.charAt(i);
+            char nextSymbol = s.charAt(i + 1);
+            if ((symbol == '(' && nextSymbol == ')') ||
+                    (symbol == '[' && nextSymbol == ']') ||
+                    (symbol == '{' && nextSymbol == '}')) {
+                state = true;
+                i++;
+            } else {
+                return false;
+            }
+
+        }
+        return state;
+    }
+}
---
+++ src/other_task/Task20ValidParenthesesCopy.java
@@ -0,0 +1,66 @@
+package other_task;
+
+import java.util.Stack;
+
+//Учитывая строку s, содержащую только символы '(', ')', '{', и '}',
+// определите, допустима ли входная строка.'['']'
+//
+//Входная строка действительна, если:
+//
+//Открытые скобки должны быть закрыты однотипными скобками.
+//Открытые скобки должны быть закрыты в правильном порядке.
+//Каждой закрывающей скобке соответствует открытая скобка того же типа.
+public class Task20ValidParenthesesCopy {
+    public static void main(String[] args) {
+        String s = "{}([))";
+        System.out.println(isValid(s));
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        // convert string into char array and access the characters using for each loop.
+        for(char ch: s.toCharArray())
+        {
+            // check ch
+            switch (ch)
+            {
+                // open bracket then push it in stack.
+                // close bracket then pop the item and compare.
+                case '(':
+                case '{':
+                case '[':
+                    stack.push(ch);
+                    break;
+                case ')':
+                    if(stack.isEmpty() || stack.pop() != '(')
+
+                    // if the stack is empty then it means string have no open bracket.
+                    // hence it is invalid.
+                    {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if(stack.isEmpty() || stack.pop() != '{')
+                    {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if(stack.isEmpty() || stack.pop() != '[')
+                    {
+                        return false;
+                    }
+                    break;
+            }
+        }
+
+
+        // After the loop we have to check one more condition.
+        // return true only if the stack is empty.
+        // if stack is not empty that means we have unused brackets.
+
+        return stack.isEmpty();
+
+    }
+}
