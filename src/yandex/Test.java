8a9219266db57b9c0375bd164e25bd2d9ee53e66
VladimirJG <vladimi85@gmail.com>
2023-04-08 23:11:56 +0300

    leet code

---
+++ src/yandex/Test.java
@@ -0,0 +1,11 @@
+package yandex;
+
+public class Test {
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.printName("Yorick");
+    }
+    public void printName(String name){
+        System.out.println("My name is " + name);
+    }
+}
