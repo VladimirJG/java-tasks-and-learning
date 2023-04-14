dbcced8121ba14ca4941485db3e8524a90e68321
VladimirJG <vladimi85@gmail.com>
2023-04-14 10:11:47 +0300

    crypto

--- src/cs50/Task5Encryption.java
+++ src/cs50/Task5Encryption.java
@@ -1,5 +1,7 @@
 package cs50;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 //Шифр Цезаря
@@ -12,27 +14,59 @@ public class Task5Encryption {
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
         int key = scanner.nextInt();
+        newEncryption(text, key);
 
 
     }
-    public static String newEncryption(String text, int key){
+
+    public static String newEncryption(String text, int key) {
         char[] lowArray = lowArrayCreate();
         char[] uppArray = uppArrayCreate();
+        List<Character> anotherSymbol = anotherSymbolArrayCreate();
         String newCryptString = "";
         for (int i = 0; i < text.length(); i++) {
             char symbol = text.charAt(i);
-            if (String.valueOf(symbol).equals(String.valueOf(symbol).toUpperCase())){
+            if (String.valueOf(symbol).equals(" ")){
+                newCryptString+=" ";
+            }
+            if (String.valueOf(symbol).equals(String.valueOf(symbol).toUpperCase())) {
                 for (int i1 = 0; i1 < uppArray.length; i1++) {
                     char uppSymbol = uppArray[i1];
-                    char encryptedSymbol = uppArray[i1+key];
-                    if (symbol == uppSymbol){
-                        newCryptString+=String.valueOf(encryptedSymbol);
+                    if (symbol == uppSymbol) {
+                        if (i1 + key >= uppArray.length) {
+                            newCryptString += String.valueOf(uppArray[i1 + key - uppArray.length]);
+                        } else {
+                            newCryptString += String.valueOf(uppArray[i1 + key]);
+                        }
                     }
                 }
-            } else if () {
-                
+            } else if (String.valueOf(symbol).equals(String.valueOf(symbol).toLowerCase())) {
+                for (int i1 = 0; i1 < lowArray.length; i1++) {
+                    char lowSymbol = lowArray[i];
+                    if (symbol == lowSymbol) {
+                        if (i1 + key >= lowArray.length) {
+                            newCryptString += String.valueOf(lowArray[i1 + key - lowArray.length]);
+                        } else {
+                            newCryptString += String.valueOf(lowArray[i1 + key]);
+                        }
+                    }
+                }
+
+            } else {
+                for (int i1 = 0; i1 < anotherSymbol.size(); i1++) {
+                    char anotherSym = anotherSymbol.get(i);
+                    if (symbol == anotherSym) {
+                        if (i1 + key >= anotherSymbol.size()) {
+                            newCryptString += String.valueOf(anotherSymbol.get(i1 + key - anotherSymbol.size()));
+                        } else {
+                            newCryptString += String.valueOf(anotherSymbol.get(i1 + key));
+                        }
+                    }
+                }
             }
         }
+        System.out.println(newCryptString);
+        return newCryptString;
     }
 
     public static char[] lowArrayCreate() {
@@ -43,6 +77,7 @@ public class Task5Encryption {
         }
         return array;
     }
+
     public static char[] uppArrayCreate() {
         char[] array = new char[26];
         for (int i1 = 0; i1 < array.length; i1++) {
@@ -50,5 +85,31 @@ public class Task5Encryption {
             array[i1] = (char) newChar;
         }
         return array;
+    }
+
+    public static List<Character> anotherSymbolArrayCreate() {
+        char[] lowArray = lowArrayCreate();
+        char[] uppArray = uppArrayCreate();
+        List<Character> listOfChars = new ArrayList<>();
+        for (int i = 0; i < 128; i++) {
+            int anotherCharSymbol = i;
+            listOfChars.add((char) anotherCharSymbol);
+        }
+        for (int i = 0; i < listOfChars.size(); i++) {
+            char listSymbol = listOfChars.get(i);
+            for (int i1 = 0; i1 < lowArray.length; i1++) {
+                char lowSymbol = lowArray[i1];
+                if (listSymbol == lowSymbol) {
+                    listOfChars.remove(listOfChars.get(i));
+                }
+            }
+            for (int i1 = 0; i1 < uppArray.length; i1++) {
+                char uppSymbol = uppArray[i1];
+                if (listSymbol == uppSymbol) {
+                    listOfChars.remove(listOfChars.get(i));
+                }
+            }
+        }
+        return listOfChars;
     }
 }
