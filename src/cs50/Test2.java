32acd08896d65614540f881dfdb3f6472529e6c7
VladimirJG <vladimi85@gmail.com>
2023-04-17 09:27:34 +0300

    cycle

--- src/cs50/Test2.java
+++ src/cs50/Test2.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class Test2 {
     public static void main(String[] args) {
         int[] arr1 = {4, 5, 8};
-        int[] arr2 = {0, 1, 2, 3, 7};
+        int[] arr2 = {0, 1, 2, 2, 3, 7};
         fillArray(arr1, arr2);
 
     }
@@ -14,14 +14,15 @@ public class Test2 {
     public static int[] fillArray(int[] a, int[] b) {
 
         int[] c = new int[a.length + b.length];
-        for (int i = 0; i < Math.max(a.length, b.length); i++) {
-            if (i >= a.length - 1) {
-                c[i] = b[i];
-            } else if (i >= b.length - 1) {
-                c[i] = a[i];
+        for (int i = 0, j = 0; i < Math.max(a.length, b.length); j++, i++) {
+            if (i >= a.length ) {
+                c[j] = b[i];
+            } else if (i >= b.length ) {
+                c[j] = a[i];
             } else {
-                c[i] = Math.min(a[i], b[i]);
-                c[i + 1] = Math.max(a[i], b[i]);
+                c[j] = Math.min(a[i], b[i]);
+                c[j + 1] = Math.max(a[i], b[i]);
+                j++;
             }
         }
         System.out.println(Arrays.toString(c));
