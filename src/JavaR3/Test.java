bd8be8ed3ee7740559c612483cab99fb8402473a
VladimirJG <vladimi85@gmail.com>
2023-04-18 09:11:20 +0300

    10 = 2

--- src/JavaR3/Test.java
+++ src/JavaR3/Test.java
@@ -1,4 +1,19 @@
 package JavaR3;
 
 public class Test {
+    public static void main(String[] args) {
+        int num = 150;
+        String str = "";
+        int[] biTable = {128, 64, 32, 16, 8, 4, 2, 1};
+        for (int i = 0; i < biTable.length; i++) {
+            int endNum = num - biTable[i];
+            if (endNum >= 0) {
+                str += "1";
+                num = endNum;
+            } else {
+                str += "0";
+            }
+        }
+        System.out.println(str);
+    }
 }
