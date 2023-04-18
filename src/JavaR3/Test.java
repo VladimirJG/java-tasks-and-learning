a7cf5e6eb0f7bb531cde661405e3ab29d5e9517b
VladimirJG <vladimi85@gmail.com>
2023-04-18 11:08:16 +0300

    10 = 2

--- src/JavaR3/Test.java
+++ src/JavaR3/Test.java
@@ -5,18 +5,22 @@ import java.util.Arrays;
 public class Test {
     public static void main(String[] args) {
         byte[] ip = new byte[]{(byte) 192, (byte) 168, 1, 2};
+        byte[] mask = new byte[]{(byte) 255, (byte) 255, (byte) 254, 0};
+        byte[] netAddress = getNetAddress(ip, mask);
         print(ip);
+        print(mask);
+    }
+    public static byte[] getNetAddress(byte[] ip, byte[] mask) {
+
+
+        return new byte[4];
     }
 
     public static void print(byte[] bytes) {
         int intByte = 0;
         String[] string = new String[bytes.length];
         for (int i = 0; i < bytes.length; i++) {
-            if (bytes[i] < 0) {
-                intByte = 256 + bytes[i];
-            } else {
-                intByte = bytes[i];
-            }
+            intByte = bytes[i] & 0xFF;
             String str = "";
             int[] biTable = {128, 64, 32, 16, 8, 4, 2, 1};
             for (int i1 = 0; i1 < biTable.length; i1++) {
--- src/JavaR3/operator/Task1.java
+++ src/JavaR3/operator/Task1.java
@@ -31,5 +31,27 @@ public class Task1 {
     }
 
     public static void print(byte[] bytes) {
+        int intByte = 0;
+        String[] string = new String[bytes.length];
+        for (int i = 0; i < bytes.length; i++) {
+            if (bytes[i] < 0) {
+                intByte = 256 + bytes[i];
+            } else {
+                intByte = bytes[i];
+            }
+            String str = "";
+            int[] biTable = {128, 64, 32, 16, 8, 4, 2, 1};
+            for (int i1 = 0; i1 < biTable.length; i1++) {
+                int endNum = intByte - biTable[i1];
+                if (endNum >= 0) {
+                    str += "1";
+                    intByte = endNum;
+                } else {
+                    str += "0";
+                }
+            }
+            string[i] = str;
+        }
+        System.out.println("//" + string[0] + " " + string[1] + " " + string[2] + " " + string[3]);
     }
 }
