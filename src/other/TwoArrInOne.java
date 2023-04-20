81a1ad137d252f0cac8c56792ff7ea4422593f6c
VladimirJG <vladimi85@gmail.com>
2023-04-20 09:17:34 +0300

    Sinergy module 3 task 1

---
+++ src/other/TwoArrInOne.java
@@ -0,0 +1,33 @@
+package other;
+
+import java.util.Arrays;
+
+public class TwoArrInOne {
+    public static void main(String[] args) {
+        int[] arr1 = {0, 7, 9, 15};
+        int[] arr2 = {1, 2, 3, 5, 8, 11, 13};
+        concatenateArr(arr1, arr2);
+    }
+
+    public static int[] concatenateArr(int[] a, int[] b) {
+        int[] endArr = new int[a.length + b.length];
+        for (int i = 0, j = 0; i < Math.max(a.length, b.length); i++, j++) {
+            if (i >= a.length) {
+                endArr[j] = b[i];
+            } else if (i >= b.length) {
+                endArr[j] = a[i];
+            } else {
+                endArr[j] = Math.min(a[i], b[i]);
+                endArr[j + 1] = Math.max(a[i], b[i]);
+                j++;
+            }
+            if (j >= 2 && endArr[j-1] < endArr[j - 2]) {
+                int n = endArr[j - 2];
+                endArr[j - 2] = endArr[j-1];
+                endArr[j-1] = n;
+            }
+        }
+        System.out.println(Arrays.toString(endArr));
+        return endArr;
+    }
+}
