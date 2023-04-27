6dd8fbf07af0d741e708d743f1a6737e469e59d2
VladimirJG <vladimi85@gmail.com>
2023-04-27 08:32:57 +0300

    LC remove duplicates

---
+++ src/other_task_leet_code/task_83/ListNode.java
@@ -0,0 +1,20 @@
+package other_task_leet_code.task_83;
+
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
---
+++ src/other_task_leet_code/task_83/Task83.java
@@ -0,0 +1,31 @@
+package other_task_leet_code.task_83;
+
+//83. Remove Duplicates from Sorted List
+public class Task83 {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1, new ListNode(1, new ListNode(2)));
+        ListNode res = deleteDuplicates(head);
+
+        while (res!=null){
+            System.out.println(res.val);
+            res = res.next;
+        }
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;
+
+        ListNode newHead = head;
+
+        while (newHead.next!=null){
+            if(newHead.val == newHead.next.val){
+                newHead.next = newHead.next.next;
+            }else {
+                newHead = newHead.next;
+            }
+        }
+        return head;
+    }
+
+}
+
