8a0e0849f7b5c2ee0682579b89cab651488fe143
VladimirJG <vladimi85@gmail.com>
2023-04-27 16:12:52 +0300

    new switch case

---
+++ src/other_task_leet_code/Test.java
@@ -0,0 +1,11 @@
+package other_task_leet_code;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(2^0);
+        double i = 2.0;
+        double m = 3.0;
+
+        System.out.println(8.0/3.0);
+    }
+}
