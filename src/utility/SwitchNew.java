f6ef0c124c8b958c832cc6f382e0810228d59036
VladimirJG <vladimi85@gmail.com>
2023-04-27 16:13:08 +0300

    new switch case

---
+++ src/utility/SwitchNew.java
@@ -0,0 +1,14 @@
+package utility;
+
+public class SwitchNew {
+    public static void main(String[] args) {
+
+        int month = 9;
+
+        String res = switch (month) {
+            case 9, 10, 11 -> "Numbers";
+            case 15-> "Return";
+            default -> throw new IllegalStateException("Unexpected value: " + month);
+        };
+    }
+}
