870a6dada321938b0c3e2eba68428f92b117e13b
VladimirJG <vladimi85@gmail.com>
2023-04-27 16:14:58 +0300

    new switch case + var

--- src/utility/SwitchNew.java
+++ src/utility/SwitchNew.java
@@ -3,7 +3,7 @@ package utility;
 public class SwitchNew {
     public static void main(String[] args) {
 
-        int month = 9;
+        var month = 9;
 
         String res = switch (month) {
             case 9, 10, 11 -> "Numbers";
