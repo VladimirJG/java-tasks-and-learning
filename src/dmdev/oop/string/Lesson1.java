67d4f6a492402e549d20d52205ac1f0b39b2c041
VladimirJG <vladimi85@gmail.com>
2023-04-28 13:35:09 +0300

    First word

--- src/dmdev/oop/string/Lesson1.java
+++ src/dmdev/oop/string/Lesson1.java
@@ -14,6 +14,18 @@ public class Lesson1 {
         String string2 = "123 hdjvb do;m; 123";
         String word = "123";
         System.out.println(startAndEndWord(string2, word));
+
+
+        /**
+         * Написать функцию, которая принимает фамилию, имя, отчество и возвращает
+         * первые символы в формате "Ф.И.О.". Регистр вводных данных не определен
+         */
+
+        String firstName = "ivan";
+        String lastName = "Kakaev";
+        String patronymic = "urugvaevich";
+        String result = firstWordOfFullName(firstName, lastName, patronymic);
+        System.out.println(result);
     }
 
     public static String replaceStr(String string) {
@@ -22,5 +34,13 @@ public class Lesson1 {
 
     private static boolean startAndEndWord(String string, String word) {
         return string.startsWith(word) && string.endsWith(word);
+    }
+
+    public static String firstWordOfFullName(String firstName, String lastName, String patronymic) {
+        char firstNameWord = Character.toUpperCase(firstName.charAt(0));
+        char lastNameWord = Character.toUpperCase(lastName.charAt(0));
+        char patronymicWord = Character.toUpperCase(patronymic.charAt(0));
+
+        return String.format("%s.%s.%s.", lastNameWord, firstNameWord, patronymicWord);
     }
 }
