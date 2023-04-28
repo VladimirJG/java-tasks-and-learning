a195e43b3bb1cb8ee5211cd2eb449137aa742807
VladimirJG <vladimi85@gmail.com>
2023-04-28 13:15:51 +0300

    end/start with

--- src/dmdev/oop/string/Lesson1.java
+++ src/dmdev/oop/string/Lesson1.java
@@ -7,9 +7,20 @@ public class Lesson1 {
     public static void main(String[] args) {
         String string = "kfjvkn :( ldjnln :( ksbkbk:) hk bksbkbck:(";
         System.out.println(replaceStr(string));
+/**
+ * Написать функцию, принимающую два параметра: строку и слово
+ * и возвращающую true если строка начинается и заканчивается этим словом
+ */
+        String string2 = "123 hdjvb do;m; 123";
+        String word = "123";
+        System.out.println(startAndEndWord(string2, word));
     }
 
     public static String replaceStr(String string) {
         return string.replace(":(", ":)");
+    }
+
+    private static boolean startAndEndWord(String string, String word) {
+        return string.startsWith(word) && string.endsWith(word);
     }
 }
