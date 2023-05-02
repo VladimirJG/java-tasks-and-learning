f6976e3d7aed7677a3372ebc9df08cbd2c83dded
VladimirJG <vladimi85@gmail.com>
2023-05-02 08:53:42 +0300

    added method attackEnemy forEach

--- src/dmdev/oop/lesson15/TrainingGround.java
+++ src/dmdev/oop/lesson15/TrainingGround.java
@@ -3,14 +3,22 @@ package dmdev.oop.lesson15;
 public class TrainingGround {
     public static void main(String[] args) {
         Hero hero1 = new Hero("Archy");
-        hero1.attackEnemy();
+        /* hero1.attackEnemy();*/
 
-        Warrior warrior1 = new Warrior("Воин Джун");
-        Mage mage1 = new Mage("Маг Магомед");
-        Archer archer1 = new Archer("Лучник Илья");
+        Hero warrior1 = new Warrior("Воин Джун");
+        Hero mage1 = new Mage("Маг Магомед");
+        Hero archer1 = new Archer("Лучник Илья");
 
-        warrior1.attackEnemy();
+       /* warrior1.attackEnemy();
         mage1.attackEnemy();
-        archer1.attackEnemy();
+        archer1.attackEnemy();*/
+
+        attackEnemy(warrior1, archer1, mage1);
+    }
+
+    public static void attackEnemy(Hero... heroes) {
+        for (Hero hero : heroes) {
+            hero.attackEnemy();
+        }
     }
 }
