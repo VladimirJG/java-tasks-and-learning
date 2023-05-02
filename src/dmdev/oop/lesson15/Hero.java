24535db1f595cb46d62bb5f4f87e415a602f2ac8
VladimirJG <vladimi85@gmail.com>
2023-05-02 08:27:47 +0300

    task

---
+++ src/dmdev/oop/lesson15/Hero.java
@@ -0,0 +1,13 @@
+package dmdev.oop.lesson15;
+
+public class Hero {
+    private String name;
+
+    public Hero(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
