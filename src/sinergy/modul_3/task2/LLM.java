2cb485ceb6709a3573b79957df80e210d8ab2703
VladimirJG <vladimi85@gmail.com>
2023-05-22 09:20:33 +0300

    synergy AL

---
+++ src/sinergy/modul_3/task2/LLM.java
@@ -0,0 +1,62 @@
+package sinergy.modul_3.task2;
+
+import java.util.AbstractList;
+import java.util.Arrays;
+
+public class LLM<T> extends AbstractList<T> {
+    private int DEFAULT_CAPACITY = 15;
+    private Object[] elementData;
+    private int size = 0;
+
+    public LLM() {
+        elementData = new Object[DEFAULT_CAPACITY];
+    }
+
+    public LLM(int capacity) {
+        elementData = new Object[capacity];
+    }
+
+
+    @Override
+    public T get(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Вводимое значение " + index + " не входит в размер массива");
+        }
+        return (T) elementData[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    private void resizing() {
+        int newSize = elementData.length + elementData.length / 2;
+        elementData = Arrays.copyOf(elementData, newSize);
+    }
+
+    @Override
+    public void add(int index, T element) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("Вводимое значение " + index + " не входит в размер массива");
+        }
+        if (index == elementData.length) {
+            resizing();
+        }
+        for (int i = size - 1; i >= index; i--) {
+            elementData[i + 1] = elementData[i];
+        }
+        elementData[index] = element;
+        size++;
+    }
+
+    @Override
+    public T remove(int index) {
+        T symbol = get(index);
+        for (int i = index; i < size - 1; i++) {
+            elementData[i] = elementData[i + 1];
+        }
+        size--;
+        return symbol;
+    }
+}
---
+++ src/sinergy/modul_3/task2/Test.java
@@ -0,0 +1,28 @@
+package sinergy.modul_3.task2;
+
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<String> list = new LLM<>();
+        list.add("Hara");
+        list.add("Injection");
+        list.add("Trouble");
+        list.add("Hara");
+        list.add("Injection");
+        list.add("Trouble");
+        list.add("Hara");
+        list.add("Injection");
+        list.add("Trouble");list.add("Hara");
+        list.add("Injection");
+        list.add("Trouble");list.add("Hara");
+        list.add("Injection");
+        list.add("Trouble");list.add("Hara");
+        list.add("Injection");
+
+        System.out.println(list.size());
+        System.out.println(list.get(1));
+        System.out.println(list.remove(2));
+        System.out.println(list.size());
+    }
+}
