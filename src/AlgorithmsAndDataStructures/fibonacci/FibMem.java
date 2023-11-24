c26d8376c67d19062e91b9266ecd838596af343f
VladimirJG <vladimi85@gmail.com>
2023-11-24 09:15:29 +0300

    fibonacci memoization

---
+++ src/AlgorithmsAndDataStructures/fibonacci/FibMem.java
@@ -0,0 +1,24 @@
+package AlgorithmsAndDataStructures.fibonacci;
+
+import java.util.Arrays;
+
+public class FibMem {
+    public static void main(String[] args) {
+        int n = 5;
+        long[] mem = new long[n + 1];
+        Arrays.fill(mem, -5);
+        System.out.println(fibMem(n, mem));
+    }
+
+    private static long fibMem(int n, long[] mem) {
+        if (n <= 1)
+            return n;
+
+        if (mem[n] != -5)
+            return mem[n];
+
+        long result = fibMem(n - 1, mem) + fibMem(n - 2, mem);
+        mem[n] = result;
+        return result;
+    }
+}
