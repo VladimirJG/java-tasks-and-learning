98b2a31d2fb13ee9bace8f976be3fd8c996d2f76
VladimirJG <vladimi85@gmail.com>
2023-11-24 08:17:07 +0300

    fibonacci

--- src/AlgorithmsAndDataStructures/fibonacci/Fibonacci.java
+++ src/AlgorithmsAndDataStructures/fibonacci/Fibonacci.java
@@ -2,12 +2,26 @@ package AlgorithmsAndDataStructures.fibonacci;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        System.out.println(fibNaive(50));
+//        System.out.println(fibNaive(3));
+        System.out.println(fibEffective(5));
     }
+
 
     private static long fibNaive(int n) {
         if (n <= 1)
             return n;
         return fibNaive(n - 1) + fibNaive(n - 2);
+    }
+
+    private static long fibEffective(int n) {
+        if (n <= 1)
+            return n;
+        long[] arr = new long[n + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+            arr[i] = arr[i - 1] + arr[i - 2];
+        return arr[n];
     }
 }
