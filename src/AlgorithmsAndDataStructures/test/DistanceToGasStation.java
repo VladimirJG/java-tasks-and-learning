67d19ef063f3a1e4d714919ed5384128767b7487
VladimirJG <vladimi85@gmail.com>
2023-11-24 14:19:30 +0300

    Gas station refactoring

--- src/AlgorithmsAndDataStructures/test/DistanceToGasStation.java
+++ src/AlgorithmsAndDataStructures/test/DistanceToGasStation.java
@@ -2,17 +2,23 @@ package AlgorithmsAndDataStructures.test;
 
 public class DistanceToGasStation {
     public static void main(String[] args) {
-        int[] distanceToThePark = new int[]{200, 375, 550, 750, 950};
+        int[] distanceToThePark = new int[]{0, 200, 375, 550, 750, 950, 1500, 1550};
         int possibleToTravel = 400;
-        System.out.println(numberOfStops(possibleToTravel,distanceToThePark));
+        System.out.println("stops done: " + numberOfStops(possibleToTravel, distanceToThePark));
     }
 
     private static int numberOfStops(int possibleToTravel, int[] distanceToThePark) {
         int stopsCount = 0;
-        for (int i = 0; i < distanceToThePark.length; i++) {
-            if (distanceToThePark[i] > possibleToTravel) {
-                possibleToTravel = possibleToTravel + distanceToThePark[i - 1];
-                stopsCount++;
+        int passed = possibleToTravel;
+        for (int i = 0; i < distanceToThePark.length - 1; i++) {
+            if (distanceToThePark[i + 1] > passed) {
+                if (distanceToThePark[i] <= passed && (distanceToThePark[i + 1] - distanceToThePark[i]) <= possibleToTravel) {
+                    passed = possibleToTravel + distanceToThePark[i];
+                    stopsCount++;
+                } else {
+                    System.out.println("next gas station not available " + distanceToThePark[i + 1]);
+                    break;
+                }
             }
         }
         return stopsCount;
