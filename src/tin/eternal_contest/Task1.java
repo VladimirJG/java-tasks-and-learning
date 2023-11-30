cf1169b1527dd402c669e7651b10fe348af14531
VladimirJG <vladimi85@gmail.com>
2023-11-30 15:53:46 +0300

    Task1

--- src/tin/eternal_contest/Task1.java
+++ src/tin/eternal_contest/Task1.java
@@ -10,7 +10,26 @@ public class Task1 {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("src", "number");
         List<Integer> numbers = inputField(file);
-        System.out.println(numbers);
+        boolean check = false;
+        for (Integer num : numbers) {
+            if (num == 0) {
+                check = true;
+                break;
+            }
+        }
+        if (check) {
+            System.out.println("Вводимые данные должны превышать значение 0");
+        } else {
+            int tariffCost = numbers.get(0);
+            int tariffSize = numbers.get(1);
+            int priceExtraMB = numbers.get(2);
+            int internetTrafficSizeNextMonth = numbers.get(3);
+
+            int excessTraffic = (internetTrafficSizeNextMonth - tariffSize);
+            if (excessTraffic > 0)
+                System.out.println(excessTraffic * priceExtraMB + tariffCost);
+            else System.out.println(tariffCost);
+        }
     }
 
     public static List<Integer> inputField(File file) {
