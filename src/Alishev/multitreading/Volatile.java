af5139f146d04a23f3f8ad84eb4870b5f0cc16a7
VladimirJG <vladimi85@gmail.com>
2024-01-19 09:57:13 +0300

    Volatile multithreading

--- src/Alishev/multitreading/Volatile.java
+++ src/Alishev/multitreading/Volatile.java
@@ -7,6 +7,9 @@ public class Volatile {
         MyClass myClass = new MyClass();
         myClass.start();
 
+        Thread thread = new Thread(new MyClass2());
+        thread.start();
+
         Scanner scanner = new Scanner(System.in);
         scanner.nextLine();
 
@@ -14,8 +17,18 @@ public class Volatile {
     }
 }
 
+class MyClass2 implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("myClass2" + i);
+        }
+    }
+}
 class MyClass extends Thread {
-    private boolean running = true;
+    private volatile boolean running = true; // добавить volatile и когерентность будет правильная - не буде в кэ
+    // 2 ядра записано неверное состояние переменной.... бля  просто вспомни когерентность.
     private int count = 0;
 
     @Override
